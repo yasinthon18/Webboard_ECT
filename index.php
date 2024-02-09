b213bfd1dd1b4c62dfa2819265c320c1025db59a
yasinthon18 <152575428+yasinthon18@users.noreply.github.com>
2024-02-09 12:12:34 +0700

    Navbar Finished
    Navbar Finished

--- index.php
+++ index.php
@@ -7,10 +7,38 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HomePage</title>
+    <link  rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
-    <h1 style="text-align: center;"> Web Yasinthon</h1>
-    <hr>
+    <div class="container-lg">
+
+    <h1 style="text-align: center;" class="mt-3"> Web Yasinthon</h1>
+
+    <nav class="navbar navbar-expand-lg" style="background-color: #d3d3d3;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><i class="bi bi-house-fill"></i> Home</a>
+      <ul class="navbar-nav">
+
+        <?php
+            if(!isset($_SESSION['id'])){ ?>
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i> เข้าสู่ระบบ</a>
+            </li>
+        <?php }else{?>
+            <li class="nav-item dropdown">
+                <a class="btn btn-outline-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi bi-person-circle"></i> <?php echo $_SESSION['username']; ?>
+                </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="logout.php"><i class="bi bi-power"></i> ออกจากระบบ</a></li>
+            </ul>
+        </li>
+        <?php }?>
+      </ul> 
+  </div>
+</nav>
     <form>
         หมวดหมู่ <select name "Category">
         <option value="all">--ทั้งหมด--</option>
@@ -42,6 +70,6 @@ session_start();
  }
 
 ?>
-
+</div>
 </body>
 </html>
